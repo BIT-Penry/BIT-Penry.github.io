0dd19f9d5492c0302af873701e8e0dbd799b564f
BIT-Penry <2772694115@qq.com>
2025-04-04 16:56:51 +0800

    Site updated: 2025-04-04 16:56:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["article/2025-4-4-第一篇文章/","article/hello-world/"];function toRandomPost(){
+var posts=["article/2025-4-4-第一篇文章/","article/hello-world/","article/2025-4-4-第一篇测试文档/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
