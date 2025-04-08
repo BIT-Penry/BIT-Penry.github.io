f80721413650e08a1c29b08b35853175126656b3
BIT-Penry <2772694115@qq.com>
2025-04-08 12:58:22 +0800

    Site updated: 2025-04-08 12:58:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["article/hello-world/","article/测试文档/","article/第一篇-hexo-anzhiyu-配置心得/"];function toRandomPost(){
+var posts=["article/hello-world/","article/测试文档/","article/第一篇-hexo-anzhiyu-配置心得/","article/第二篇-博文写作语法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
