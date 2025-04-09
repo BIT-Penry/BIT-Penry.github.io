625d892393557f468406a380bbcb171d76170af4
BIT-Penry <2772694115@qq.com>
2025-04-09 19:29:22 +0800

    Site updated: 2025-04-09 19:29:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["article/hello-world/","article/测试文档/","article/第一章-Latex与Vscode的相遇/","article/第二篇-博文写作语法/","article/第一篇-hexo-anzhiyu-配置心得/","article/第三篇-Twikoo评论邮件回复魔改/"];function toRandomPost(){
+var posts=["article/hello-world/","article/测试文档/","article/第一章-Latex与Vscode的相遇/","article/第三篇-Twikoo评论邮件回复魔改/","article/第一篇-hexo-anzhiyu-配置心得/","article/第二篇-博文写作语法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
