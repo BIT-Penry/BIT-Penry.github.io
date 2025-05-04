7c449d103e94fdb08e811c004e5e948e00b7c66b
BIT-Penry <2772694115@qq.com>
2025-05-04 12:26:22 +0800

    Site updated: 2025-05-04 12:26:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["article/hello-world/","article/测试文档/","article/第一章-Latex与Vscode的相遇/","article/第一篇-hexo-anzhiyu-配置心得/","article/第三章-biber-VS-bibtex/","article/第三篇-Twikoo评论邮件回复魔改/","article/第二章-Vscode-PK-Overleaf/","article/第二篇-博文写作语法/","article/第四篇-Hexo-魔改实录-给博客添个“藏宝阁”，美观又实用！/","article/第四章-Hexo-GitHub-Pages-从零配置自动部署（SSH-版）/"];function toRandomPost(){
+var posts=["article/hello-world/","article/测试文档/","article/第一章-Latex与Vscode的相遇/","article/第一篇-hexo-anzhiyu-配置心得/","article/第三章-biber-VS-bibtex/","article/第三篇-Twikoo评论邮件回复魔改/","article/第二章-Vscode-PK-Overleaf/","article/第二篇-博文写作语法/","article/第四篇-Hexo-魔改实录-给博客添个“藏宝阁”，美观又实用！/","article/第四章-Hexo-GitHub-Pages-从零配置自动部署（SSH-版）/","article/第一篇-Gitee-PicGo图床配置指南：Hexo-博客写作的高效插图解决方案/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
