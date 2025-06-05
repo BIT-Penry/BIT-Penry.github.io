9d8d22a5d2c390acaa9a79ad3d1298039d208ca8
BIT-Penry <2772694115@qq.com>
2025-06-05 12:15:03 +0800

    Site updated: 2025-06-05 12:15:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["article/hello-world/","article/测试文档/","article/Zotero-1/","article/LaTeX-1/","article/LaTeX-2/","article/Hexo-0/","article/Hexo-2/","article/Hexo-3/","article/Hexo-4/","article/LaTeX-3/","article/Tools-1/","article/Hexo-1/"];function toRandomPost(){
+var posts=["article/hello-world/","article/测试文档/","article/Zotero-1/","article/LaTeX-1/","article/LaTeX-2/","article/Hexo-0/","article/Hexo-2/","article/Hexo-3/","article/Hexo-4/","article/LaTeX-3/","article/Tools-1/","article/Hexo-1/","article/Linear-Algebra/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
