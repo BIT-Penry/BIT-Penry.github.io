0333e4ef982ead254963e12519d9961372856c03
BIT-Penry <2772694115@qq.com>
2025-06-09 22:40:45 +0800

    Site updated: 2025-06-09 22:40:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["article/Hexo-0/","article/Hexo-1/","article/Hexo-2/","article/Hexo-3/","article/Hexo-4/","article/LaTeX-1/","article/LaTeX-2/","article/LaTeX-3/","article/Linear-Algebra/","article/Tools-1/","article/hello-world/","article/Zotero-1/","article/测试文档/","article/Python-Basic-Learning/","article/Python-numpy-1/"];function toRandomPost(){
+var posts=["article/Hexo-0/","article/Hexo-1/","article/Hexo-2/","article/Hexo-3/","article/LaTeX-1/","article/LaTeX-2/","article/LaTeX-3/","article/Hexo-4/","article/Python-Basic-Learning/","article/Tools-1/","article/Python-numpy-1/","article/测试文档/","article/Linear-Algebra/","article/Zotero-1/","article/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
